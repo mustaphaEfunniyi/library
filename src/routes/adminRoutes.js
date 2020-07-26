1374e75d930bd776958a61c49893b28642c8f0c0
Efunniyi Mustapha <mefunniyi@yahoo.com>
2020-07-26 05:16:43 +0100

    Connected to MongoDB

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+const debug = require('debug')('app:adminRoutes');
+
+const { MongoClient } = require('mongodb');
+
+const adminRouter = express.Router();
+const books = [
+  {
+    title: 'The Wind in the willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Graham',
+    read: false,
+  },
+  {
+    title: 'The Wind in the willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Graham',
+    read: false,
+  },
+  {
+    title: 'The Wind in the willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Graham',
+    read: false,
+  },
+  {
+    title: 'The Wind in the willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Graham',
+    read: false,
+  },
+  {
+    title: 'The Wind in the willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Graham',
+    read: false,
+  },
+  {
+    title: 'The Wind in the willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Graham',
+    read: false,
+  },
+];
+
+function router(nav) {
+  adminRouter.route('/').get((req, res) => {
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('connected correctly to the server');
+
+        const db = client.db(dbName);
+
+        const response = await db.collection('books').insertMany(books);
+        res.json(response);
+      } catch (err) {
+        debug(err.stack);
+      }
+
+      client.close();
+    }());
+
+  });
+  return adminRouter;
+}
+
+module.exports = router;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,64 +1,67 @@
 const express = require('express');
 
+const debug = require('debug')('app:bookRoutes');
+
+const { MongoClient, ObjectID } = require('mongodb');
+
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [
-    {
-      title: 'The Wind in the willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Graham',
-      read: false,
-    },
-    {
-      title: 'The Wind in the willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Graham',
-      read: false,
-    },
-    {
-      title: 'The Wind in the willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Graham',
-      read: false,
-    },
-    {
-      title: 'The Wind in the willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Graham',
-      read: false,
-    },
-    {
-      title: 'The Wind in the willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Graham',
-      read: false,
-    },
-    {
-      title: 'The Wind in the willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Graham',
-      read: false,
-    },
-  ];
-
   bookRouter.route('/').get((req, res) => {
-    res.render('bookListView', {
-      nav,
-      title: 'Library',
-      books,
-    });
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('connected correctly to the server');
+
+        const db = client.db(dbName);
+
+        const col = await db.collection('books');
+        const books = await col.find().toArray();
+        res.render('bookListView', {
+          nav,
+          title: 'Library',
+          books,
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      client.close();
+    }());
   });
 
   bookRouter.route('/:id').get((req, res) => {
     const { id } = req.params;
-    res.render('bookView', {
-      nav,
-      title: 'Library',
-      book: books[id],
-    });
-  });
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
 
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('connected correctly to the server');
+
+        const db = client.db(dbName);
+
+        const col = await db.collection('books');
+
+        const book = await col.findOne({ _id: new ObjectID(id) });
+
+        debug(book);
+        
+        res.render('bookView', {
+          nav,
+          title: 'Library',
+          book
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+    }());
+  });
   return bookRouter;
 }
 
